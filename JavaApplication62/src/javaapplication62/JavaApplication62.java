7c9f00133eaab7e6bee3c31223fc14361c954c16
Camilo Sáez <Camilo Sez@CamiloSez-PC>
2014-04-07 16:33:24 -0300

    Mi segundo commit.

--- JavaApplication62/src/javaapplication62/JavaApplication62.java
+++ JavaApplication62/src/javaapplication62/JavaApplication62.java
@@ -15,6 +15,6 @@ public class JavaApplication62 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(":D");
+        System.out.println(":g");
     }
 }
